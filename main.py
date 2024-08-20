4ec4b8cc835207bc74993a4671c2bc3126bc636e
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-20 16:27:24 -0400

    update gitignore

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from fpdf import FPDF
 
 pdf = FPDF(orientation="portrait", unit="mm", format="A4")
+
+pdf.set_font(family="helvetica", size=16)
+pdf.add_page()
+
+# You can add text through cells
+pdf.cell(w=0, h=12, txt="Hello world 1", align="L", ln=1, border=1)
+pdf.cell(w=0, h=12, txt="Hi there 2", align="L", ln=1, border=1)
+
+pdf.output("output.pdf")
+print("PDF successfully created")
